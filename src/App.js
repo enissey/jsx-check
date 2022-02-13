8bc845bea21b6d9824cb4635c4d02214afb7dcbe
yassine <yassinechatti0123@gmail.com>
2022-02-13 22:26:28 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import img from './imageInSrc.jpg'
+import './Style.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+       <h1 className="title red">Yassine Chatti</h1>
+  </div>
       </header>
+      <div>
+      <img src={img} ></img> 
+  <img src='imageInPublic.jpg'></img>
+  </div>
     </div>
   );
 }
